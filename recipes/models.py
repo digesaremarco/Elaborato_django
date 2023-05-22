33e08065c80870a79ed4c6f7c4aaac24e35e50cf
digesaremarco <digesaremarco@gmail.com>
2023-05-22 10:50:15 +0200

    Renamed "posts" into "recipes"

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
+class Recipe(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     categorie = models.ForeignKey('categories.Category', on_delete=models.CASCADE)
