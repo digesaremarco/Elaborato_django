52c78afae3a0b158453574fba17567ebe7907950
digesaremarco <digesaremarco@gmail.com>
2023-05-24 22:00:27 +0200

    Updated db

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    categorie = models.ForeignKey('categories.Category', on_delete=models.CASCADE)
+    category = models.ForeignKey('categories.Category', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
