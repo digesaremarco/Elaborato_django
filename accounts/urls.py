518d8fd66ba5d1c476efe2ff0e7253ec72fd0cbf
digesaremarco <digesaremarco@gmail.com>
2023-05-31 12:41:02 +0200

    Updated user signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUp
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/', SignUp, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,35 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from requests import request
+
+from accounts.forms import UserForm
 
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+def SignUp1(request):
+    #form_class = UserForm
+    #success_url = reverse_lazy('login')
+    #template_name = 'registration/signup.html'
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            return redirect('login')
+    else:
+        form = UserForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+
+def SignUp(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
