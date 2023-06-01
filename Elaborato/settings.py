405ea2d41ebfbd24a975d4ca756b90c54b95b52c
digesaremarco <digesaremarco@gmail.com>
2023-06-01 23:21:58 +0200

    Fix fo railway

--- Elaborato/settings.py
+++ Elaborato/settings.py
@@ -137,3 +137,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TIME_ZONE = 'Europe/Rome'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Elaborato/static')]
