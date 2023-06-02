c6ad4e925e9b0f60cd1faf6ea2da388779d4e208
digesaremarco <digesaremarco@gmail.com>
2023-06-02 17:47:41 +0200

    changed settings

--- Elaborato/settings.py
+++ Elaborato/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-osanezwu0ka+z*t4qd!o++7+)9%6*h-3kalvwq%g_)g9yh!ez9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG',) != 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
